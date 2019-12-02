30c523a7a8f64ff97a6d8952d40a853a2f616ea1
Slamdoom <slamdoom@slam.doom>
2019-12-02 18:07:01 +0000

    gitgnore build files

---
+++ cpp_source_for_orbslam/copy_to_lib_source_dir.py
@@ -0,0 +1,21 @@
+from glob import glob
+import os
+from shutil import copyfile
+
+src_dir = "/slamdoom/tmp/orbslam2/src/"
+inc_dir = "/slamdoom/tmp/orbslam2/include/"
+
+
+fs_cc = glob(os.path.dirname(__file__) + "/*.cc")
+fs_h = glob(os.path.dirname(__file__) + "/*.h")
+
+for f in fs_cc:
+    fname = f.split("/")[-1]
+    o = src_dir + "/" + fname
+    copyfile(f, o)
+
+
+for f in fs_h:
+    fname = f.split("/")[-1]
+    o = inc_dir + "/" + fname
+    copyfile(f, o)
