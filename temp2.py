ba9ff7afec96fae7658dcc1bc887db8714e44ffb
Slamdoom <slamdoom@slam.doom>
2020-07-11 07:58:16 +0000

    changes cpp includes in SP-slam

--- temp2.py
+++ temp2.py
@@ -1,30 +1,17 @@
-from multiprocessing import Process, Queue
-import time
-import sys
+import os
+from pprint import pprint
 
-def reader_proc(queue):
-    ## Read from the queue; this will be spawned as a separate Process
-    while True:
-        msg = queue.get()         # Read from the queue and do nothing
-        if (msg == 'DONE'):
-            break
-
-def writer(count, queue):
-    ## Write to the queue
-    for ii in range(0, count):
-        queue.put(ii)             # Write 'count' numbers into the queue
-    queue.put('DONE')
-
-if __name__=='__main__':
-    pqueue = Queue() # writer() writes to pqueue from _this_ process
-    for count in [10**4, 10**5, 10**6]:
-        ### reader_proc() reads from pqueue as a separate process
-        reader_p = Process(target=reader_proc, args=((pqueue),))
-        reader_p.daemon = True
-        reader_p.start()        # Launch reader_proc() as a separate python process
-
-        _start = time.time()
-        writer(count, pqueue)    # Send a lot of stuff to reader()
-        reader_p.join()         # Wait for the reader to finish
-        print("Sending {0} numbers to Queue() took {1} seconds".format(count,
-                                                                       (time.time() - _start)))
+walker = os.walk("/SP-SLAM/Thirdparty/")
+for d, ds, fs in walker:
+    for f in fs:
+        with open(d + "/" + f, 'r') as conn:
+            lns = conn.read()
+        # pprint(lns)
+        if '#include "../../config.h"' in lns:
+            print(d + "/" + f)
+            lns = lns.replace('#include "../../config.h"', '#include "Config.h"')
+            with open(d + "/" + f, 'w') as conn:
+                conn.write(lns)
+        # print(type(lns))
+        # exit()
+# print(next(iter(walker)))
